6770420b59589079e06771e8afeff79ecf0d88f1
nsf03 <nazarova.sf@phystech.edu>
2026-04-05 20:57:55 +0300

    update styles and urls for improved layout and organization

--- myproject/main/urls.py
+++ myproject/main/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('test/', views.test, name='test'),  # <-- добавляем маршрут
+    path('test/', views.test, name='test'),  #добавляем маршрут
     path('organelle/<str:organelle_type>/', views.organelle_detail, name='organelle_detail'),
 ]
